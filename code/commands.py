18363dd1a260ba0936fb4c42969c0efa8e5259a9
Enrique Paulino <enrique-paulino@outlook.com>
2022-06-14 00:43:43 +0100

    Added comments and organised all code into seperate functions.

--- code/commands.py
+++ code/commands.py
@@ -2,7 +2,7 @@ import re
 from word2number import w2n
 
 
-def split_list(lst, word):
+def split_list(lst, word):  # Splits a list of words by specific word
     idx_list = [idx + 1 for idx, val in
                 enumerate(lst) if val == word]
     result = [lst[i: j] for i, j in
@@ -12,45 +12,7 @@ def split_list(lst, word):
     return result
 
 
-def take(command, window):
-    if 'new' in command:
-        query = re.search('new (.+)', command)
-        if query:
-            widget = query.group(1).split()
-            try:
-                widget = split_list(widget, 'text')
-                widget_command = '_'.join(widget[0])
-                widget_text = ' '.join(widget[1])
-                func = getattr(__import__('widgets'), 'widget_'+widget_command)
-                func(window, widget_text)
-            except Exception:
-                try:
-                    func = getattr(__import__('widgets'),
-                                   'widget_'+widget[0])
-                    func(window, '')
-                except Exception as e:
-                    print('Error, please try again.')
-                    print(e)
-
-    elif 'move' in command:
-        x = window.winfo_children()
-        query = re.search('move (.+)', command)
-        if query:
-            # If there is a number convert it, but if there isnt let it be name
-            try:
-                widget = query.group(1).split()
-                widget, direction = find_widget(widget)
-                for i in x:
-                    # Fix for first widget created of its type has no number
-                    if ('.!' + widget) == str(i) + '1':
-                        move(i, direction)
-                    if widget in str(i):
-                        move(i, direction)
-            except Exception:
-                pass
-
-
-def find_widget(widget):
+def find_widget(widget):  # Function to find widget name (and direction)
     widget_name = widget[0]
     try:
         direction = widget[2]+widget[3]
@@ -70,7 +32,7 @@ def find_widget(widget):
     return widget, direction
 
 
-def move(widget, direction):
+def move(widget, direction):  # Function to position widget
     match direction:
         case 'top':
             return widget.pack(side='top', anchor='n')
@@ -91,3 +53,49 @@ def move(widget, direction):
             return widget.pack(side='bottom', anchor='sw')
         case 'bottomright':
             return widget.pack(side='bottom', anchor='se')
+
+
+def new_command(window, command):  # Command to create new widget
+    query = re.search('new (.+)', command)
+    if query:
+        widget = query.group(1).split()
+        try:
+            widget = split_list(widget, 'text')
+            widget_command = '_'.join(widget[0])
+            widget_text = ' '.join(widget[1])
+            func = getattr(__import__('widgets'), 'widget_'+widget_command)
+            func(window, widget_text)
+        except Exception:
+            try:
+                func = getattr(__import__('widgets'),
+                               'widget_'+widget[0])
+                func(window, '')
+            except Exception as e:
+                print('Error, please try again.')
+                print(e)
+
+
+def move_command(window, command):  # Command to move specific widget
+    x = window.winfo_children()
+    query = re.search('move (.+)', command)
+    if query:
+        # If there is a number convert it, but if there isnt let it be name
+        try:
+            widget = query.group(1).split()
+            widget, direction = find_widget(widget)
+            for i in x:
+                # Fix for first widget created of its type has no number
+                if ('.!' + widget) == str(i) + '1':
+                    move(i, direction)
+                if widget in str(i):
+                    move(i, direction)
+        except Exception:
+            pass
+
+
+def take(command, window):  # Main function
+    if 'new' in command:
+        new_command(window, command)
+
+    elif 'move' in command:
+        move_command(window, command)
