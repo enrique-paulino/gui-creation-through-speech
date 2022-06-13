aef88760f7035e323f9d3357a5742eb4c8f45c6b
Enrique Paulino <enrique-paulino@outlook.com>
2022-06-13 11:48:40 +0100

    Implemented WIP move command - center.

--- code/commands.py
+++ code/commands.py
@@ -2,13 +2,13 @@ import re
 from word2number import w2n
 
 
-def split_list(lst):
+def split_list(lst, word):
     idx_list = [idx + 1 for idx, val in
-                enumerate(lst) if val == 'text']
+                enumerate(lst) if val == word]
     result = [lst[i: j] for i, j in
               zip([0] + idx_list, idx_list
                   + ([len(lst)] if idx_list[-1] != len(lst) else []))]
-    result[0].remove('text')
+    result[0].remove(word)
     return result
 
 
@@ -17,7 +17,7 @@ def take(command, window):
         query = re.search('new (.+)', command)
         if query:
             widget = query.group(1).split()
-            widget = split_list(widget)
+            widget = split_list(widget, 'text')
             widget_command = '_'.join(widget[0])
             widget_text = ' '.join(widget[1])
             print(widget_text)
@@ -27,15 +27,28 @@ def take(command, window):
             except Exception as e:
                 print(e)
 
-    """elif 'move' in command:
+    elif 'move' in command:
         x = window.winfo_children()
-        print(x)
         query = re.search('move (.+)', command)
         if query:
-            widget = query.group(1).split()
-            widget_name = widget[0]
-            widget_number = str(w2n.word_to_num(widget[1]))
-            widget = widget_name + widget_number
-            print(widget)
-            values = [s for s in x if any(xs in s for xs in [widget])]
-            print(values)"""
+            # If there is a number convert it, but if there isnt let it be name
+            try:
+                widget = query.group(1).split()
+                widget_name = widget[0]
+
+                # Temporary fix for speech recognizing two as to
+                if (widget[1] == 'to'):
+                    widget[1] = 'two'
+
+                widget_number = str(w2n.word_to_num(widget[1]))
+                widget = widget_name + widget_number
+
+                for i in x:
+                    # Fix for first widget created of its type has no number
+                    if ('.!' + widget) == str(i) + '1':
+                        i.place(relx=0.5, rely=0.5, anchor='center')
+                    if widget in str(i):
+                        i.place(relx=0.5, rely=0.5, anchor='center')  # Center
+
+            except Exception:
+                pass
